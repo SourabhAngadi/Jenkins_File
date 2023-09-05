f1c8e522e67db70ddd114c95fd69026f67d727fe
SourabhAngadi <141898486+SourabhAngadi@users.noreply.github.com>
2023-09-05 15:20:44 +0530

    commit

---
+++ src/test/java/TestNG9.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestNG9 {
+	WebDriver driver;
+  @Test
+  public void f() {
+	  driver = new ChromeDriver();
+  }
+}
